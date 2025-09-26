324266360ab414bc0c4880c503c9823686d94ba9
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2025-09-26 17:51:33 +0100

    💄my budget - content

--- lib/screens/shoppingBag/shoppingBag.dart
+++ lib/screens/shoppingBag/shoppingBag.dart
@@ -30,7 +30,77 @@ class _ShoppingBagState extends State<ShoppingBagScreen> {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: SingleChildScrollView(),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 88,
+                            margin: const EdgeInsets.symmetric(horizontal: 8),
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Color(0xFF212121),
+                              borderRadius: BorderRadius.circular(25),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withValues(
+                                    alpha: 0.2,
+                                  ), // cor da sombra
+                                  spreadRadius: 0, // espalhamento
+                                  blurRadius: 8, // suavidade
+                                  offset: const Offset(
+                                    0,
+                                    4,
+                                  ), // desloca só para baixo
+                                ),
+                              ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                // Coluna 1: alinhado à esquerda
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "\$ 1,345",
+                                      style: Global.semiboldLengthHealdine
+                                          .copyWith(color: Colors.white),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Text(
+                                      "September forecast \$2,010",
+                                      style: Global.regular1Caption.copyWith(
+                                        color: Colors.white,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                                // Coluna 2: alinhado à direita, centralizado verticalmente
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "June, 2020",
+                                      style: Global.regular1Caption.copyWith(
+                                        color: Colors.white,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
